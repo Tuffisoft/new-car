4a8548d136b0c43203e0ecad92be172e482b7d04
Fi <vancetti@googlemail.com>
2023-03-25 17:18:59 +0100

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ export default function App() {
 
   return (
     <div className=" bg-slate-200">
+      <div className="flex justify-between items-center p-10 text-center text-slate-500 shadow-lg font-karla text-4xl ">
+      <img src="./public/fred.png" className="w-24"></img>
       <h1
-        className="p-10 text-center text-slate-500 shadow-lg font-karla text-4xl">
+        className="pr-32">
         Neues Auto 2023
-      </h1>
+        </h1>
+        </div>
       <Cars />
     </div>
   )
