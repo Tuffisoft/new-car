8041ba42b447ec0483d10569dcda3ce15b697198
Fi <vancetti@googlemail.com>
2023-03-26 16:35:42 +0200

    add 25-29

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className=" bg-slate-200">
       <div className="flex justify-between items-center p-10 text-center text-slate-500 shadow-lg font-karla text-4xl ">
-      <img src="./public/fred.png" className="w-24"></img>
+      <img src="https://i.postimg.cc/kXh82dm7/fred.png" className="w-24"></img>
       <h1
         className="pr-32">
         Neues Auto 2023
